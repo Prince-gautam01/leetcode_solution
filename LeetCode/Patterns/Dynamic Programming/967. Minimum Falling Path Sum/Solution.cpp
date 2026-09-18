f6379eecd768d9d54ed9b9f4dda4c9e354f106c7
Prince Gautam <princegautam72bth@gmail.com>
2026-09-18 16:47:24 +0530

    Add Medium C++ Solution for 967. Minimum Falling Path Sum from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/967. Minimum Falling Path Sum/Solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    private:
+    int solve(int i,int j,vector<vector<int>>& matrix,int m,int n,vector<vector<int>> &dp){
+
+        if(i>=m || j>=n || i<0 || j<0){
+            return INT_MAX;
+        }
+        if(i>=m-1){
+            return matrix[i][j];
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+
+
+        int below=solve(i+1,j-1,matrix,m,n,dp);
+        int DiagLeft=solve(i+1,j,matrix,m,n,dp);
+        int DiagRight=solve(i+1,j+1,matrix,m,n,dp);
+
+        dp[i][j]=matrix[i][j] + min(min(below,DiagLeft),DiagRight);
+
+        return dp[i][j];
+    }
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        int m=matrix.size();
+        int n=matrix[0].size();
+       int ans=INT_MAX;
+
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+
+        for(int j=0;j<n;j++){
+            ans=min(ans,solve(0,j,matrix,m,n,dp));
+        }
+      
+
+        return ans;
+        
+    }
+};
