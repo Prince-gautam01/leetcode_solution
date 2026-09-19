ac0fa92fa0356ef587d51f2d534f9ce26ed9a8a3
Prince Gautam <princegautam72bth@gmail.com>
2026-09-19 17:51:52 +0530

    Add Easy C++ Solution for 2706. Buy Two Chocolates from LeetCode

--- LeetCode/Patterns/Greedy/2706. Buy Two Chocolates/Solution.cpp
+++ LeetCode/Patterns/Greedy/2706. Buy Two Chocolates/Solution.cpp
@@ -2,18 +2,12 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
         int temp=money;
-        int cnt=0;
-        for(int i=0;i<prices.size();i++){
-            if(prices[i]<=money){
-                money=money-prices[i];
-                cnt++;
 
-                if(cnt==2){
-                    return money;
-                }
-            }
+        sort(prices.begin(),prices.end()); 
+        if((prices[0]+prices[1])<=money){
+            money=money-(prices[0]+prices[1]);
+            return money;
         }
-        
-        return temp;
+        return temp;   
     }
 };
