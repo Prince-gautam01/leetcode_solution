473234fff5905256fc7b7da96363c4dfc2186af7
Prince Gautam <princegautam72bth@gmail.com>
2026-09-21 11:24:44 +0530

    Add Medium C++ Solution for 63. Unique Paths II from LeetCode

--- LeetCode/Patterns/Dynamic Programming/63. Unique Paths II/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/63. Unique Paths II/Solution.cpp
@@ -1,10 +1,15 @@
 class Solution {
     private:
     int solve(int i,int j,vector<vector<int>>& obstacleGrid,int m,int n, vector<vector<int>> &dp){
+    
+     if(obstacleGrid[m-1][n-1]==1){
+        return 0;
+    }
     if(i>=m || j>=n || i<0 || j<0){
         return 0;
     }
-    if(i>=m-1 && j>=n-1){
+
+    if(i==m-1 && j==n-1){
         return 1;
     }
 
