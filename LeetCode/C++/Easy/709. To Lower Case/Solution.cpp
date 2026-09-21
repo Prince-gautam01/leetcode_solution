c9eeec15c03f98dbe350a2bf52bcc8ca82b01326
Prince Gautam <princegautam72bth@gmail.com>
2026-09-21 22:32:39 +0530

    Add Easy C++ Solution for 709. To Lower Case from LeetCode

---
+++ LeetCode/C++/Easy/709. To Lower Case/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+
+        string temp="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='A' && s[i]<='Z'){
+                char ch=s[i] + 32;
+                temp.push_back(ch);
+            }
+            else if(s[i]>='a' && s[i]<='z'){
+                char ch=s[i];
+                temp.push_back(ch);
+            }
+
+        }
+
+        return temp;
+        
+    }
+};
